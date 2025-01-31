f4666de2d93f7f7ce7d755ea6558596df1e1ccb1
Aaron Ager <aaronager369@gmail.com>
2025-01-31 14:41:53 -0500

    Created project structure

---
+++ CardGame_Interactive/CardGameInteractive/CardDeck.cs
@@ -0,0 +1,12 @@
+namespace CardGameInteractive;
+/// <summary>
+/// Defines the card deck as a list of cards
+/// </summary>
+public class CardDeck
+{
+    /// <summary>
+    /// List of cards in the deck
+    /// </summary>
+    private List<Card> _cardList;
+    
+}
---
+++ CardGame_Interactive/CardGameInteractive/Score.cs
@@ -0,0 +1,17 @@
+namespace CardGameInteractive;
+
+/// <summary>
+/// Define the score of the game
+/// </summary>
+public struct Score
+{
+    /// <summary>
+    /// The number of rounds won by the player
+    /// </summary>
+    private int _playerScore;
+    
+    /// <summary>
+    /// The number ow rounds won by the player
+    /// </summary>
+    private int _houseScore;
+}
